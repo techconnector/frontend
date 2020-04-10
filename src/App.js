365c597f06f8e843d9d0d91b3a3d93beea22236c
Jonatan Flores <ti02@alfasistemas.inf.br>
2020-04-10 12:06:05 -0300

    setup redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Provider } from "react-redux";
 
 import "./global.css";
 
+import store from "./store";
 import Routes from "./Routes";
 
 function App() {
-  return <Routes />;
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  );
 }
 
 export default App;
